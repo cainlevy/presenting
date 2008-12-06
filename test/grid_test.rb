1cee04992fab97d896f1ce27e63c0b56d98f2a07
Lance Ivy <lance@cainlevy.net>
2008-12-05 22:18:55 -0800

    organizing grid tests a bit better

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -7,27 +7,47 @@ class GridTest < Test::Unit::TestCase
   end
 
   def test_adding_multiple_fields
-    @g.fields = [:a, :b, :c]
+    @g.fields = [
+      :a,
+      {:b => "foo"},
+      {:c => {:value => "bar"}}
+    ]
     assert_equal 3, @g.fields.size, "all fields are saved"
-    assert_equal ['a', 'b', 'c'], @g.fields.map(&:name), "fields remain in order"
+    
+    assert_equal "a", @g.fields[0].name
+    assert_equal :a, @g.fields[0].value
+    
+    assert_equal "b", @g.fields[1].name
+    assert_equal "foo", @g.fields[1].value
+    
+    assert_equal "c", @g.fields[2].name
+    assert_equal "bar", @g.fields[2].value
   end
-  
+
+end
+
+class GriedFieldSetTest < Test::Unit::TestCase
+
+  def setup
+    @f = Presenting::Grid::FieldSet.new
+  end
+
   def test_adding_a_field_by_name
-    @g.fields = ["foo"]
-    assert_equal "foo", @g.fields.first.name, "name is stringified"
-    assert_equal "foo", @g.fields.first.value, "value is assumed to be a method"
+    @f << "foo"
+    assert_equal "foo", @f.first.name, "name is stringified"
+    assert_equal "foo", @f.first.value, "value is assumed to be a method"
   end
   
   def test_adding_a_field_by_name_and_value
-    @g.fields = ["foo" => :bar]
-    assert_equal "foo", @g.fields.first.name, "key is name"
-    assert_equal :bar, @g.fields.first.value, "value is value"
+    @f << {"foo" => :bar}
+    assert_equal "foo", @f.first.name, "key is name"
+    assert_equal :bar, @f.first.value, "value is value"
   end
   
   def test_adding_a_field_by_name_and_options
-    @g.fields = ["foo" => {:value => :bar}]
-    assert_equal "foo", @g.fields.first.name, "key is name"
-    assert_equal :bar, @g.fields.first.value, "value is found and saved"
+    @f << {"foo" => {:value => :bar}}
+    assert_equal "foo", @f.first.name, "key is name"
+    assert_equal :bar, @f.first.value, "value is found and saved"
   end
 
 end
@@ -49,4 +69,5 @@ class GridFieldTest < Test::Unit::TestCase
     assert_equal "foo", @f.name, "name remains a string"
     assert_equal "foo", @f.value, "value is assumed to be a constant string"
   end
+  
 end
