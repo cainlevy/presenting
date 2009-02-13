5f656016d6cbb6edf80e087ff4357328ae861e45
Lance Ivy <lance@cainlevy.net>
2009-02-13 02:03:37 -0800

    stylesheets controller will actually return a stylesheet

--- app/controllers/presentation/stylesheets_controller.rb
+++ app/controllers/presentation/stylesheets_controller.rb
@@ -1,6 +1,11 @@
 class Presentation::StylesheetsController < ActionController::Base
+  # TODO: page caching
+  # TODO: support comma-delimited list of stylesheets (bundling)
+
   def show
-    # TODO: returns the named stylesheets. page caching.
-    render :nothing => true
+    dir = File.join(File.dirname(__FILE__), '..', '..', 'assets', 'stylesheets')
+    respond_to do |type|
+      type.css {render :text => File.read("#{dir}/#{params[:id]}.css")}
+    end
   end
 end
--- test/assets_test.rb
+++ test/assets_test.rb
@@ -14,4 +14,10 @@ class Presentation::StylesheetsControllerTest < ActionController::TestCase
   def test_named_stylesheet_routes
     assert_equal "/presentation/stylesheets/foo.css", presentation_stylesheet_path("foo", :format => 'css')
   end
+  
+  def test_retrieving_a_named_stylesheet
+    get :show, :id => 'grid'
+    assert_response :success
+    assert_equal @response.body, File.read(File.join(PLUGIN_ROOT, 'app', 'assets', 'stylesheets', 'grid.css'))
+  end
 end
