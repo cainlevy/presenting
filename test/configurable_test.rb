cef29a35aeb35666e3d6d048d284865107b5e275
Lance Ivy <lance@cainlevy.net>
2009-02-13 02:19:24 -0800

    test coverage for Presenting::Configurable

---
+++ test/configurable_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class ConfigurableTest < Test::Unit::TestCase
+  class ConfigurableUser
+    include Presenting::Configurable
+    attr_accessor :first_name
+    attr_reader   :name
+  end
+  
+  def test_setting_first_name_with_initializer
+    user = ConfigurableUser.new(:first_name => "Bob")
+    assert_equal 'Bob', user.first_name
+  end
+  
+  def test_setting_unsettable_attribute
+    assert_raises NoMethodError do
+      ConfigurableUser.new(:name => 'Bob Jenkins')
+    end
+  end
+end
