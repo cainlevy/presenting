81b3a11e96bd8a3c28178de61d7a9ff790b8ce42
Lance Ivy <lance@cainlevy.net>
2009-02-21 14:09:00 -0800

    todo update

--- app/controllers/presentation/assets_controller.rb
+++ app/controllers/presentation/assets_controller.rb
@@ -1,4 +1,5 @@
 class Presentation::AssetsController < ActionController::Base
+  # TODO: consider packaging a minifier so we get the perfect solution: a cached, minified bundle of assets
   caches_page :stylesheet, :javascript
 
   def stylesheet
@@ -10,6 +11,20 @@ class Presentation::AssetsController < ActionController::Base
     end
   end
   
+  # TODO: bundle unobtrusive javascripts that can add behavior.
+  # - jquery vs prototype (i'll develop jquery, and wait for prototype contributions)
+  # - ajax links with html response
+  # - - inline
+  # - - modal dialog
+  # - ajax links with js response
+  # - ajax links with no response
+  # - inline editing
+  # - "dirty" form awareness
+  # - tabbed forms
+  # - tooltips for extended information (e.g. column description or truncated field text)
+  # - basic form validation
+  # - - required fields
+  # TODO: tests for ujs
   def javascript
     dir = asset_path(:javascript)
     script = params[:id].split(',').collect{ |id| File.read("#{dir}/#{id}.js") }.join("\n")
