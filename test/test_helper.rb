0154a8ad8348f4142f9e4fe7816e03af310fc8d1
Lance Ivy <lance@cainlevy.net>
2012-06-04 12:35:28 -0400

    add mocha to gemfile to fix and simplify loading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,8 @@
-# bootstrap
-require 'rubygems'
-require 'test/unit'
-require 'mocha'
-
 # load the test app, which will load gems
 ENV['RAILS_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), 'r3', 'config', 'environment.rb')
 require 'rails/test_help'
+require 'mocha'
 
 Rails.backtrace_cleaner.remove_silencers!
 
